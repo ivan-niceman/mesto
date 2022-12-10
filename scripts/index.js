4b135c83c70d836b42151a2a7959127e6bed000e
ivan-niceman <sn.iv@bk.ru>
2022-12-10 21:53:54 +0700

    fix: исправлены замечания, доработан код с пометкой 'можно лучше'

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,37 @@
 const editButton = document.querySelector('.profile__button-edit');
 const popUp = document.querySelector('.popup');
 const popUpClose = document.querySelector('.popup__button-close');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const popUpForm = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_name');
+const specialityInput = document.querySelector('.popup__input_speciality');
+const popUpInput = document.querySelectorAll('.popup__input');
 
+function inputReturn() {
+  nameInput.value = profileTitle.textContent;
+  specialityInput.value = profileSubtitle.textContent;
+}
 
 editButton.addEventListener('click', function(){
   popUp.classList.add('popup_opened');
+  inputReturn()
 })
 popUpClose.addEventListener('click', function(){
   popUp.classList.remove('popup_opened');
+  inputReturn();
 })
-popUp.addEventListener('click', function(element){
-  if(element.target == element.currentTarget) {
-    popUp.classList.remove('popup_opened');
-  }
-})
-
-const popUpForm = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__name');
-const specialityInput = document.querySelector('.popup__speciality');
+// popUp.addEventListener('click', function(element){
+//   if(element.target == element.currentTarget) {
+//     popUp.classList.remove('popup_opened');
+//     inputReturn();
+//   }
+// })
 
 function handleFormSubmit(event) {
   event.preventDefault();
-
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubtitle = document.querySelector('.profile__subtitle')
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = specialityInput.value;
-
   popUp.classList.remove('popup_opened');
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = specialityInput.value;
 }
 popUpForm.addEventListener('submit', handleFormSubmit);
